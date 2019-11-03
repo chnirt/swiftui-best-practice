7b39639b6b5567ef33626eac9acf83ad1b24e89e
chin trinh <trinhchinchin@gmail.com>
2019-11-03 19:38:34 +0700

    Initial Commit

---
+++ demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demo
+//
+//  Created by Trinh Chin on 11/3/19.
+//  Copyright © 2019 Chnirt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
